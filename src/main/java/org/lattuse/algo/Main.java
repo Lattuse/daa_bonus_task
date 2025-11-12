c2c9ef2ff4efbfdbda527d91879616ba27ccd515
Lattuse <sultik280805@gmail.com>
2025-11-13 01:43:00 +0500

    forgot to remove tips

--- src/main/java/org/lattuse/algo/Main.java
+++ src/main/java/org/lattuse/algo/Main.java
@@ -1,7 +1,5 @@
 package org.lattuse.algo;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import org.lattuse.algo.algorithm.KruskalMST;
 import org.lattuse.algo.model.Edge;
 
